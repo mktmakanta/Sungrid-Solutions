05a4103a889f53ed63e9829e075e35d191aa60a0
mktmakanta <mktmakantamkt@gmail.com>
2024-08-16 18:10:41 +0100

    done routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import MainPage from "./MainPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Resume from "./resume";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/resume",
+    element: <Resume />,
+  },
+  {
+    path: "*",
+    element: <div> page not found</div>,
+  },
+]);
 
 export default function App() {
-  return (
-    <div>
-      <MainPage />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
